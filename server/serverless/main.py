f9960bb780bb2949d4c437d0c3d8364ea7a9f356
neelk <neelesh.karthi@gmail.com>
2024-11-26 12:55:21 -0500

    increase iterations to 1000

--- server/serverless/main.py
+++ server/serverless/main.py
@@ -3,11 +3,11 @@ import time
 from utils import predict, pre_process
 
 if __name__ == '__main__':
-    filename = 'abacus.jpg'
-    with open("model_results.csv", "w") as f:
-        f.write("preprocess_time,predict_time\n")
+    filename = "abacus.jpg"
+    with open("serverless.csv", "w") as f:
+        f.write("start_time,preprocess_time,predict_time\n")
 
-        for i in range(100):
+        for i in range(1000):
             start_time = time.perf_counter()
 
             preprocessed_input = pre_process(filename)
@@ -16,5 +16,5 @@ if __name__ == '__main__':
             prediction, probability = predict(preprocessed_input)
             predict_time = time.perf_counter()
 
-            f.write(f"{preprocess_time - start_time},{predict_time - preprocess_time}\n")
+            f.write(f"{start_time},{preprocess_time},{predict_time}\n")
 
